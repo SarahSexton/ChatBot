b695d737ae89ef7670f3b539f1f97b375ac3a764
Sarah <sarahjeannesexton@live.com>
2016-04-18 11:14:52 -0500

    Initial commit

---
+++ node_modules/botbuilder/lib/consts.js
@@ -0,0 +1,17 @@
+exports.Data = {
+    SessionState: 'BotBuilder.Data.SessionState',
+    SessionId: 'BotBuilder.Data.SessionId',
+    Handler: 'BotBuilder.Data.Handler',
+    Group: 'BotBuilder.Data.Group',
+    Intent: 'BotBuilder.Data.Intent',
+    WaterfallStep: 'BotBuilder.Data.WaterfallStep'
+};
+exports.DialogId = {
+    Prompts: 'BotBuilder.Dialogs.Prompts'
+};
+exports.Id = {
+    DefaultGroup: 'BotBuilder.Id.DefaultGroup'
+};
+exports.Intents = {
+    Default: 'BotBuilder.Intents.Default'
+};
